744fae73e7c6c235154e803c47bd8ec83458d7dc
Martin <dhhwang89@gmail.com>
2019-01-08 22:55:07 +0900

    travis test

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+
+def something(i):
+
+    return i
+
+if __name__ == "__main__":
+    print("Hello World")
+
+    for i in range(100):
+
+
+
+        print(something(i))
+        
+
